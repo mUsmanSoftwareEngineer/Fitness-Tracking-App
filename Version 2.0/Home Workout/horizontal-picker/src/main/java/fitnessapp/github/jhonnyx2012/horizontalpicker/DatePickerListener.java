85b597e85e46aa5906661760734bb4c0e2f952c1
mUsmanSoftwareEngineer <127174452+mUsmanSoftwareEngineer@users.noreply.github.com>
2024-01-10 22:56:06 +0500

    Updated Home Workout

---
+++ Version 2.0/Home Workout/horizontal-picker/src/main/java/fitnessapp/github/jhonnyx2012/horizontalpicker/DatePickerListener.java
@@ -0,0 +1,10 @@
+package fitnessapp.github.jhonnyx2012.horizontalpicker;
+
+import org.joda.time.DateTime;
+
+/**
+ * Created by jhonn on 02/03/2017.
+ */
+public interface DatePickerListener {
+    void onDateSelected(DateTime dateSelected);
+}
